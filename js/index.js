08481130e2aea81d0299248e65e1d3f154cb6ea8
Ahmad Malkawi <malkawi_ahmad@hotmail.com>
2024-05-04 00:50:43 +0300

    scroll btn

--- js/index.js
+++ js/index.js
@@ -45,17 +45,18 @@ if(localStorage.stock != null){
 create.onclick = function(e){
     e.preventDefault()
     let newStock = {
-        StockName : StockName.value,
+        StockName : StockName.value.toLowerCase(),
         Price : Price.value,
         Taxes : Taxes.value,
         Ads : Ads.value,
         Discount : Discount.value,
         total : total.innerHTML,
-        Category : Category.value,
+        Category : Category.value.toLowerCase(),
         Count : Count.value
         
     }
-    if (mood === 'Create') {
+    if(StockName.value && Price.value && Category.value != '' && Count.value <= 100){
+      if (mood === 'Create') {
       if(newStock.Count > 1)
     {
         for (let i = 0; i < newStock.Count; i++) {
@@ -67,19 +68,21 @@ create.onclick = function(e){
         stockData.push(newStock);
 
     }  
+    clearData();
     }else{
         stockData[ variable ] = newStock;
         mood = 'Create'
         create.innerHTML = 'Create'
         Count.style.display = 'block'
         countLabel.style.display = 'block'
-    }
+    }}
+    
     
 
 
     localStorage.setItem('stock', JSON.stringify(stockData));
     showData()
-    clearData();
+   
     
 }
 
@@ -176,3 +179,94 @@ function deleteAllData(){
 
 }
 
+//search
+let searchMood = 'stockName';
+function getSearchMood(id) {
+    let search = document.getElementById('search')
+    if( id == 'searchByStockName' ){
+        searchMood = 'stockName';
+
+    }
+    else
+    {
+        searchMood = 'category';
+
+    }
+    search.placeholder = 'Search By '+ searchMood
+    search.value = '';
+    showData()
+    search.focus();
+    window.scroll({
+        top:900,
+    })
+}
+function searchData(value){
+    
+    let table = ''
+    for(let i = 0;i<stockData.length;i++){
+    if(searchMood == 'stockName'){
+        
+            if (stockData[i].StockName.includes(value.toLowerCase())) {
+                table+= `
+                <tr>
+                    <td>${i+1}</td>
+                    <td>${stockData[i].StockName}</td>
+                    <td>${stockData[i].Price}</td>
+                    <td>${stockData[i].Taxes}</td>
+                    <td>${stockData[i].Ads}</td>
+                    <td>${stockData[i].Discount}</td>
+                    <td>${stockData[i].total}</td>
+                    <td>${stockData[i].Category}</td>
+                    <td><button onclick="updateData(${i})" class="btn btn-primary createBtn tableBtns " id="updateBtn">Update</button></td>
+                    <td><button onclick="deleteData(${i})" class="btn btn-primary createBtn tableBtns " id="deleteBtn">Delete</button></td>
+                </tr>
+        `    
+            
+        }
+    }
+    else
+    {
+        
+            if (stockData[i].Category.includes(value.toLowerCase())) {
+                table+= `
+                <tr>
+                    <td>${i+1}</td>
+                    <td>${stockData[i].StockName}</td>
+                    <td>${stockData[i].Price}</td>
+                    <td>${stockData[i].Taxes}</td>
+                    <td>${stockData[i].Ads}</td>
+                    <td>${stockData[i].Discount}</td>
+                    <td>${stockData[i].total}</td>
+                    <td>${stockData[i].Category}</td>
+                    <td><button onclick="updateData(${i})" class="btn btn-primary createBtn tableBtns " id="updateBtn">Update</button></td>
+                    <td><button onclick="deleteData(${i})" class="btn btn-primary createBtn tableBtns " id="deleteBtn">Delete</button></td>
+                </tr>
+        `    
+            }
+        
+
+    }
+}
+    document.getElementById('tbody').innerHTML = table;
+
+}
+
+let btn = document.getElementById('btn');
+window.onscroll= function(){
+    if(scrollY >= 400){
+        btn.style.display = 'block'
+        
+    }
+    else{
+        btn.style.display = 'none'
+    }
+
+}
+
+btn.onclick = function () {
+    scroll({
+        top:180,
+        left:0,
+        behavior:'smooth',
+    })
+}
