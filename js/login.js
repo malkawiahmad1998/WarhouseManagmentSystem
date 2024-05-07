4746ea1fbfb0c008bff9d54e54b20c2ee81a1411
Ahmad Malkawi <malkawi_ahmad@hotmail.com>
2024-05-07 19:21:36 +0300

    update

--- js/login.js
+++ js/login.js
@@ -58,6 +58,7 @@ signIn.onclick = function(e){
   e.preventDefault();
   for (let i = 0; i < AllUsers.length; i++) {
     if(loginName.value == AllUsers[i].registerEmail && loginPassword.value == AllUsers[i].registerPassword){
+    if(AllUsers[i].registerEmail === loginName.value && AllUsers[i].registerPassword === loginPassword.value  ){
       
       localStorage.setItem('isLoggedIn',true)
       window.location.href = 'index.html'
